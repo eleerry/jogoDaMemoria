ab7836cd3ce7a4d6ee63802f0564327255f886ed
eleerry <luisblack.lr@gmail.com>
2022-06-11 19:27:46 -0300

    parte 4

--- js/app.js
+++ js/app.js
@@ -1,9 +1,27 @@
 const cards = document.querySelectorAll('.card')
+let hasFlippedCard = false
+let firstCard, secondCard
 
 function flipCard(){
-    this.classList.toggle('flip')
+    this.classList.add('flip')
+
+    if(!hasFlippedCard){
+        hasFlippedCard = true
+        firstCard = this
+        return
+    }
+
+    secondCard = this
+    checkForMath()
 }
 
-cards.forEach( card=>{
+function checkForMath(){
+    if(firstCard.dataSet.card === secondCard.dataSet.card){
+        disabledCards()
+        return
+    }
+}
+
+cards.forEach( card =>{
     card.addEventListener('click', flipCard)
 })
