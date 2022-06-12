c0e672bd41cb8ec9e8cd9ceb89d928d4280b23c2
eleerry <luisblack.lr@gmail.com>
2022-06-11 21:30:06 -0300

    parte 5

--- js/app.js
+++ js/app.js
@@ -4,8 +4,10 @@ let firstCard, secondCard
 let lockBoard = false
 
 function flipCard(){
+
     if(lockBoard) return
-    
+    if(this === firstCard) return
+
     this.classList.add('flip')
 
     if(!hasFlippedCard){
@@ -30,13 +32,23 @@ function checkForMath(){
 function disabledCards(){
     firstCard.removeEventListener('click', flipCard)
     secondCard.removeEventListener('click', flipCard)
+
+    resetBoard()
 }
 
 function unflipCards(){
+    lockBoard = true
     setTimeout( () => {
         firstCard.classList.remove('flip')
         secondCard.classList.remove('flip')
+
+        resetBoard()
     }, 1500)
+}
+
+function resetBoard(){
+    [hasFlippedCard, lockBoard] = [false, false]
+    [firstCard, secondCard] = [null, null]
 }
 
 cards.forEach( card =>{
